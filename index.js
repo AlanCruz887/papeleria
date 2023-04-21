feefcc1ace58c955bd2087105fd7ba6139f1252f
Alan Cruz <acruzdelangel887@gmail.com>
2023-04-21 07:41:47 -0600

    Se creo el servidor

---
+++ index.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const app = express()
+
+app.set('port',process.env.PORT||3600);
+app.listen(app.get('port'),()=>{
+    console.log("Servidor en el puerto " + app.get('port'))
+})
