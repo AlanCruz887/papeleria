048da314c3e2575b61821da9613a8006a28ba4c6
Alan Cruz <acruzdelangel887@gmail.com>
2023-04-24 09:52:19 -0600

    rutas

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
-const express = require('express')
+const express = require('express');
+const morgan = require('morgan');
 const app = express()
-
+//settings
 app.set('port',process.env.PORT||3600);
+
+
+//middlewares
+app.use(morgan('dev'))
+
+
+//rutas
+app.get('/:v',(req,res)=>{
+  const valor = req.params.v
+  let t='';
+  for (let index = 0; index < 11; index++) {
+   t+=(valor + " x " + index + " = " + index*valor)
+  }
+  res.send(t)
+  
+ 
+})
+
 app.listen(app.get('port'),()=>{
     console.log("Servidor en el puerto " + app.get('port'))
 })
